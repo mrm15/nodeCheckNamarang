adddce2262620a234aff0579a351e6cedc31e0f2
mrm15 <mrm.learn@gmail.com>
2024-02-18 11:48:09 +0330

    may be later i want to add toast

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,24 @@ import useObjectDataHolder from "./Hooks/useObjectDataHolder.jsx";
 import {initialObjectState} from "./util/initials.js";
 import {userVerified} from "./util/userVerified.js";
 import Login from "./Login/Login.jsx";
+// import { ToastContainer } from "react-toastify";
+
 
 function App() {
   const [fullData, setFullData] = useObjectDataHolder({...initialObjectState})
   return (<>
+    {/*<ToastContainer*/}
+    {/*  position="bottom-left"*/}
+    {/*  autoClose={2500}*/}
+    {/*  hideProgressBar={true}*/}
+    {/*  newestOnTop={false}*/}
+    {/*  theme="colored"*/}
+    {/*  closeOnClick*/}
+    {/*  rtl*/}
+    {/*  pauseOnFocusLoss={false}*/}
+    {/*  draggable*/}
+    {/*  pauseOnHover*/}
+    {/*/>*/}
     <MainContext.Provider value={{setFullData, fullData}}>
       {!userVerified() ? <Login/> : <ProductNode/>}
     </MainContext.Provider>
