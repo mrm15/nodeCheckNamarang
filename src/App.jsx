dfc0ef8bec6e92a3ef56c8a6f697ba033ea474f2
mrm15 <mrm.learn@gmail.com>
2024-02-18 10:19:37 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import "./style.scss"
 import {MainContext} from "./Context/Context.jsx";
 import useObjectDataHolder from "./Hooks/useObjectDataHolder.jsx";
 import {initialObjectState} from "./util/initials.js";
+import {userVerified} from "./util/userVerified.js";
+import Login from "./Login/Login.jsx";
 
 function App() {
   const [fullData, setFullData] = useObjectDataHolder({...initialObjectState})
-
   return (<>
-      <MainContext.Provider value={{setFullData, fullData}}>
-        <ProductNode/>
-      </MainContext.Provider>
-
-    </>)
+    <MainContext.Provider value={{setFullData, fullData}}>
+      {!userVerified() ? <Login/> : <ProductNode/>}
+    </MainContext.Provider>
+  </>)
 }
 
 export default App
