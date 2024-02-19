19ca379562d6fe13be9199b1d19f2598ebbc64e4
mrm15 <mrm.learn@gmail.com>
2024-02-19 11:59:45 +0330

    Update Modal.jsx

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -9,7 +9,9 @@ const MyModal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className={` fixed inset-0 flex items-center justify-center ${isOpen ? 'block' : 'hidden'}`}>
-      <div className='fixed inset-0 bg-black opacity-50'></div>
+      <div className='fixed inset-0 bg-black opacity-50'
+      // onClick={onClose}
+      ></div>
       <div className={`bg-white rounded-lg p-8 z-10 w-auto`}>
         <div className="flex justify-end">
           <button className="text-gray-600 hover:text-gray-800" onClick={onClose}><FaTimes /></button>
